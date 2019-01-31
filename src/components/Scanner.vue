d57bd86a16e91be7922422beb2c4ce41a3bcb911
trickypr <trickypr@users.noreply.github.com>
2019-01-31 16:36:12 +1100

    Add vue

---
+++ src/components/Scanner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="scanner"><h1>test</h1></div>
+</template>
+
+<script>
+export default {
+  name: "scanner",
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- src/scripts/instascan/index.js
+++
@@ -1,8 +0,0 @@
-import Scanner from 'instascan/src/scanner'
-
-import Camera from 'instascan/src/camera'
-
-export default {
-  Scanner: Scanner,
-  Cammera: Camera
-}
